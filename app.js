a9f972363a3a2ffa42de5df20ea0b972bf38adc9
Ian Kaire <iankaire@gmail.com>
2024-05-07 14:11:12 +0300

    eventEmitter module setup

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-//FS Module
-const fs = require('fs');
+//Event Emitter Module
+const EventEmitter = require('node:events');
 
-fs.readdir('./', (err, files)=>{
-      if(err) console.log(`Error: ${err}`);
-      else console.log(`Result: ${files}`);
+const eventEmitterA = new EventEmitter();
+const eventEmitterB = new EventEmitter();
+
+//Listen to an event
+eventEmitterA.on('Message logged', () => {
+     console.log('Your message');
 });
+
+//Raise an event
+eventEmitterA.emit("Message logged");
+
+
+//Listen to an event before emitting it 'the order matters'
+
+//Event Arguments
+eventEmitterB.on('Arguments', (arg) => {
+    console.log('Your arguments', arg);
+});
+
+//Raise an event
+eventEmitterB.emit("Arguments", {id: 1, url: 'https:/'});
 
